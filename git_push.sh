0b06dc019cce0f3df63b380fe58e501b0151cadc
PauloGomesNatixis <paulo.gomes@natixis.com>
2024-09-19 15:16:36 +0100

    15:16

--- git_push.sh
+++ git_push.sh
@@ -2,6 +2,8 @@
 
 current_date=$(date +'%Y-%m-%d_%H:%M:%S')
 
+git config --get-all user.name PauloGomesNatixis
+git config --get-all user.email paulo.gomes@natixis.com
 
 git add .
 git commit -m "Commit_$current_date"
