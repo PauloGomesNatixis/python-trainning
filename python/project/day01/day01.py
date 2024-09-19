ecc82f4a2526e6bf8ffc0890cf10f52a973d7f79
PauloGomesNatixis <paulo.gomes@natixis.com>
2024-09-19 14:56:59 +0100

    Commit_2024-09-19_14:56:58

--- python/project/day01/day01.py
+++ python/project/day01/day01.py
@@ -1,1 +1,8 @@
-print ("OLA")
+from labs.lab02 import print_lab02
+
+def main():
+    print_lab02()
+    
+    
+main()
+    
---
+++ python/project/day01/labs/lab02.py
@@ -0,0 +1,7 @@
+
+def print_lab02():
+    print ("LAB02")
+
+
+if __name__ == "__main__" :
+    print_lab02()
