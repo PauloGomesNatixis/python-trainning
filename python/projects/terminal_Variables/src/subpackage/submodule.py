2dc4af62f9eaa92aa03482d5007bd5f4d1cf9290
PauloGomesNatixis <paulo.gomes@natixis.com>
2024-09-20 17:28:38 +0100

    17:28

--- python/projects/terminal_Variables/src/subpackage/submodule.py
+++ python/projects/terminal_Variables/src/subpackage/submodule.py
@@ -1,17 +1,17 @@
 class Terminal with the following methods:
-  - `__init__`: Initializes the class with the current working directory.
-  - `list_files`: Lists all files and directories in the current directory.
-  - `create_directory`: Creates a new directory.
-  - `rename_directory`: Renames a directory.
-  - `remove_directory`: Removes a directory.
+  __init__ # Initializes the class with the current working directory.
+  def list_files: # Lists all files and directories in the current directory.
+  def create_directory: #Creates a new directory.
+  def rename_directory: #Renames a directory.
+  def remove_directory: #Removes a directory.
   
   
   
   
-  class named `Environment` with the following methods:
-  - `__init__`: Initializes the class with the value of an environment variable.
-  - `get_variable`: Gets the value of an environment variable.
-  - `set_variable`: Sets the value of an environment variable.
+  class Environment:
+  __init__ # Initializes the class with the value of an environment variable.
+  def get_variable # Gets the value of an environment variable.
+  def set_variable # Sets the value of an environment variable.
   
   
   
