8ff4f38072c2cf31d6b393fcf52935b1ce496a41
PauloGomesNatixis <paulo.gomes@natixis.com>
2024-09-20 17:32:07 +0100

    20/09/2024 17:31

---
+++ python/projects/terminal_Variables/src/main.py
@@ -0,0 +1,13 @@
+
+""" 
+Create a function named `main` that:
+  - Creates an instance of the `Terminal` class.
+  - Lists all files and directories in the current directory.
+  - Creates a new directory.
+  - Renames the directory.
+  - Removes the directory.
+  - Creates an instance of the `Environment` class.
+  - Gets the value of an environment variable.
+  - Sets the value of an environment variable. """
+  
+  
